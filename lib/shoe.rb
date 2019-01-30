3effe3899e058b76456967046ac95cf7fdc6c919
A Glazer <avigayilglazer@gmail.com>
2019-01-30 20:28:14 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -5,7 +5,7 @@ attr_accessor :brand, :color, :size, :material, :condition
     @brand = brand
   end
 
-  def cobble(condition)
+  def cobble
     puts "Your shoe is as good as new!"
     condition == "new"
   end
